45d39c22944ae3fd363a19f2793ea325f6ebfbaa
febus <artem.dzichkanets@mail.ru>
2025-07-11 23:04:57 +0300

    Add a PrintRoutes function

--- lib/core/RouteService.cpp
+++ lib/core/RouteService.cpp
@@ -1,1 +1,18 @@
 #include "RouteService.h"
+
+#include <iostream>
+
+#include "../api/RouteAPI.h"
+#include "RouteFactory.h"
+
+void PrintRoutes(const std::string& departure_yandex_code, const std::string& destination_yandex_code,
+    const std::chrono::year_month_day date) {
+
+    RouteAPI api_object;
+    // std::cout << api_object.FetchRouteJSON(departure_yandex_code, destination_yandex_code, date) << std::endl;
+    const auto api_response_json = api_object.FetchRouteJSON(departure_yandex_code, destination_yandex_code, date);
+    const auto routes = MakeRoutes(api_response_json);
+    for (const auto& route : routes) {
+        std::cout << route->GetInfo() << std::endl;
+    }
+}
--- lib/core/RouteService.h
+++ lib/core/RouteService.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <chrono>
+#include <string>
+
+void PrintRoutes(const std::string& departure_yandex_code, const std::string& destination_yandex_code, std::chrono::year_month_day date);
+
+std::string GetCityCode(std::string& city_title);
